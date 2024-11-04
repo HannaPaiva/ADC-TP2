7528dc6a3b6ac835c2b38a57fa839e5ddc0f2ff0
Adriano Muncaciu <muncaciua54@gmail.com>
2024-11-04 17:01:29 +0000

    merge main into my branch

---
+++ Controller/leitor.py
@@ -0,0 +1,70 @@
+from bd_connector import criar_conexao, fechar_conexao
+
+def adicionar_leitor(nome, morada, telefone, nif, email):
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    try:
+        cursor.execute("INSERT INTO Leitores (nome, morada, telefone, nif, email) VALUES (?, ?, ?, ?, ?)", 
+                       (nome, morada, telefone, nif, email))
+        conexao.commit()
+        print("Leitor adicionado com sucesso.")
+    except sqlite3.Error as e:
+        print(f"Erro ao adicionar leitor: {e}")
+    fechar_conexao(conexao)
+
+
+def listar_leitores():
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM Leitores")
+    leitores = cursor.fetchall()
+    fechar_conexao(conexao)
+    return leitores
+
+
+def atualizar_leitor(numero_leitor, nome=None, morada=None, telefone=None, nif=None, email=None):
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    campos = []
+    valores = []
+
+    if nome:
+        campos.append("nome = ?")
+        valores.append(nome)
+    if morada:
+        campos.append("morada = ?")
+        valores.append(morada)
+    if telefone:
+        campos.append("telefone = ?")
+        valores.append(telefone)
+    if nif:
+        campos.append("nif = ?")
+        valores.append(nif)
+    if email:
+        campos.append("email = ?")
+        valores.append(email)
+
+    if campos:
+        valores.append(numero_leitor)
+        sql = f"UPDATE Leitores SET {', '.join(campos)} WHERE numero_leitor = ?"
+        try:
+            cursor.execute(sql, valores)
+            conexao.commit()
+            print("Leitor atualizado com sucesso.")
+        except sqlite3.Error as e:
+            print(f"Erro ao atualizar leitor: {e}")
+    else:
+        print("Nenhum campo para atualizar.")
+    
+    fechar_conexao(conexao)
+    
+def deletar_leitor(numero_leitor):
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    try:
+        cursor.execute("DELETE FROM Leitores WHERE numero_leitor = ?", (numero_leitor,))
+        conexao.commit()
+        print("Leitor deletado com sucesso.")
+    except sqlite3.Error as e:
+        print(f"Erro ao deletar leitor: {e}")
+    fechar_conexao(conexao)
