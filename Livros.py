50b147442d3dcc9939957555c986dee8ef4f76ff
Paulo Ferreira <a85285@ualg.pt>
2024-11-04 16:36:27 +0000

    esqueleto inicial da feature de livros

---
+++ Livros.py
@@ -0,0 +1,23 @@
+import sqlite3
+from app import conexao
+
+
+# Função que lista todos os livros na tabela Livros
+def listar_livros():
+# A Fazer
+
+# Função que lista o livro desejado através do ISBN
+def listar_livros_id(isbn):
+# A Fazer
+
+# Função que adiciona um novo livro à base de dados
+def adicionar_livro(isbn, titulo, autor, categoria, ano_publicacao):
+# A Fazer
+
+# Função que atualiza os dados de um livro com base no ISBN fornecido
+def atualizar_livro(isbn):
+# A Fazer
+
+# Função que apaga um livro da base de dados através do ISBN
+def apagar_livro(isbn):
+# A Fazer
