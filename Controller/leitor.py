22b28c45a6c5286d3a63d41f937dacaea5b30f81
Joao Cunha <joaocunhask@gmail.com>
2024-11-04 15:45:51 +0000

    added: atualizar leitores

--- Controller/leitor.py
+++ Controller/leitor.py
@@ -21,3 +21,40 @@ def listar_leitores():
     fechar_conexao(conexao)
     return leitores
 
+
+def atualizar_leitor(numero_leitor, nome=None, morada=None, telefone=None, nif=None, email=None):
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    campos = []
+    valores = []
+
+    if nome:
+        campos.append("nome = ?")
+        valores.append(nome)
+    if morada:
+        campos.append("morada = ?")
+        valores.append(morada)
+    if telefone:
+        campos.append("telefone = ?")
+        valores.append(telefone)
+    if nif:
+        campos.append("nif = ?")
+        valores.append(nif)
+    if email:
+        campos.append("email = ?")
+        valores.append(email)
+
+    if campos:
+        valores.append(numero_leitor)
+        sql = f"UPDATE Leitores SET {', '.join(campos)} WHERE numero_leitor = ?"
+        try:
+            cursor.execute(sql, valores)
+            conexao.commit()
+            print("Leitor atualizado com sucesso.")
+        except sqlite3.Error as e:
+            print(f"Erro ao atualizar leitor: {e}")
+    else:
+        print("Nenhum campo para atualizar.")
+    
+    fechar_conexao(conexao)
+
