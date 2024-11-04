1d49106d3267b865151b1f73f13cacaef2f7005d
Joao Cunha <joaocunhask@gmail.com>
2024-11-04 15:44:02 +0000

    added: listar leitores

--- Controller/leitor.py
+++ Controller/leitor.py
@@ -11,3 +11,13 @@ def adicionar_leitor(nome, morada, telefone, nif, email):
     except sqlite3.Error as e:
         print(f"Erro ao adicionar leitor: {e}")
     fechar_conexao(conexao)
+
+
+def listar_leitores():
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM Leitores")
+    leitores = cursor.fetchall()
+    fechar_conexao(conexao)
+    return leitores
+
