86916fe7a801f9354bacd85893d67807b855c537
Paulo Ferreira <a85285@ualg.pt>
2024-11-04 16:45:12 +0000

    adicao da funcionalidade de procurar todos os livros na base de dados, adicao de uma funcao de conexao, e remoção de um ficheiro que estava a mais

--- Livros.py
+++ Livros.py
@@ -1,23 +1,38 @@
 import sqlite3
-from app import conexao
-
+# from app import conexao
+def conexao():
+    conn = sqlite3.connect('base_de_dados.db')
+    return conn
 
 # Função que lista todos os livros na tabela Livros
 def listar_livros():
-# A Fazer
+    conn = conexao()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Livros')
+    livros = cursor.fetchall()
+    conn.close()
+    return livros
 
 # Função que lista o livro desejado através do ISBN
 def listar_livros_id(isbn):
-# A Fazer
+    # A Fazer
+    print("Por desenvolver...")
 
 # Função que adiciona um novo livro à base de dados
 def adicionar_livro(isbn, titulo, autor, categoria, ano_publicacao):
-# A Fazer
+    # A Fazer
+    print("Por desenvolver...")
 
 # Função que atualiza os dados de um livro com base no ISBN fornecido
 def atualizar_livro(isbn):
-# A Fazer
+    # A Fazer
+    print("Por desenvolver...")
 
 # Função que apaga um livro da base de dados através do ISBN
 def apagar_livro(isbn):
-# A Fazer
+    # A Fazer
+    print("Por desenvolver...")
+
+
+
+listar_livros()
