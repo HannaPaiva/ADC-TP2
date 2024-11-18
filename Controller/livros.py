7f242f287b70640eaa95947618dbec7666a6d875
Paulo Ferreira <a85285@ualg.pt>
2024-11-18 16:28:49 +0000

    vamos ver se vai agora

---
+++ Controller/livros.py
@@ -0,0 +1,68 @@
+from bd_connector import criar_conexao, fechar_conexao
+
+def adicionar_livro(isbn, titulo, autor, categoria, ano_publicacao):
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    try:
+        cursor.execute("INSERT INTO Livros (isbn, titulo, autor, categoria, ano_publicacao) VALUES (?,?,?,?,?)",(isbn,titulo,autor,categoria,ano_publicacao))
+        conexao.commit()
+        print("Livro adicionado com sucesso.")
+    except sqlite3.Error as e:
+        print(f"Erro ao adicionar livro: {e}")
+    fechar_conexao(conexao)
+
+def listar_livro():
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM Livros")
+    livros = cursor.fetchall()
+    fechar_conexao(conexao)
+    return livros
+
+
+def atualizar_livro(isbn, titulo=None, autor=None, categoria=None, ano_publicacao=None):
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    campos = []
+    valores = []
+
+    if isbn:
+        campos.append("isbn = ?")
+        valores.append(isbn)
+    if titulo:
+        campos.append("titulo = ?")
+        valores.append(titulo)
+    if autor:
+        campos.append("autor = ?")
+        valores.append(autor)
+    if categoria:
+        campos.append("categoria = ?")
+        valores.append(categoria)
+    if ano_publicacao:
+        campos.append("ano publicacao = ?")
+        valores.append(ano_publicacao)
+
+    if campos:
+        valores.append(isbn)
+        sql = f"UPDATE Livros SET {', '.join(campos)} WHERE isbn = ?"
+        try:
+            cursor.execute(sql, valores)
+            conexao.commit()
+            print("Livro atualizado com sucesso.")
+        except sqlite3.Error as e:
+            print(f"Erro ao atualizar leitor: {e}")
+    else:
+        print("Nenhum campo para atualizar.")
+    
+    fechar_conexao(conexao)
+    
+def deletar_livro(isbn):
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    try:
+        cursor.execute("DELETE FROM Livros WHERE isbn = ?", (isbn,))
+        conexao.commit()
+        print("Livro deletado com sucesso.")
+    except sqlite3.Error as e:
+        print(f"Erro ao deletar leitor: {e}")
+    fechar_conexao(conexao)
---
+++ menu_livro.py
@@ -0,0 +1,45 @@
+from Controller.livros import *
+
+def gerenciar_livros():
+    while True:
+        print("\nGerenciar Livros")
+        print("1. Adicionar Livro")
+        print("2. Listar Livro")
+        print("3. Atualizar Livro")
+        print("4. Deletar Livro")
+        print("5. Voltar ao Menu Principal")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == '1':
+            isbn = input("ISBN: ")
+            titulo = input("Titulo do Livro: ")
+            autor = input("Autor do Livro: ")
+            categoria = input("Categoria do Livro: ")
+            ano_publicacao = input("Ano de Publicação do Livro: ")
+            adicionar_livro(isbn, titulo, autor, categoria, ano_publicacao)
+
+        elif opcao == '2':
+            livros = listar_livro()
+            print("\nLista de Livros:")
+            for livro in livros:
+                print(livros)
+
+        elif opcao == '3':
+            isbn = int(input("ISBN do livro a atualizar: "))
+            print("Deixe o campo em branco para não atualizar o valor.")
+            titulo = input("Novo titulo do livro: ")
+            autor = input("Novo autor do livro: ")
+            categoria = input("Nova categoria do livro: ")
+            ano_publicacao = input("Novo ano de pulicação do livro: ")
+            atualizar_livro(isbn, titulo or None, autor or None, categoria or None, ano_publicacao or None)
+
+        elif opcao == '4':
+            isbn = int(input("ISBN do livro a deletar: "))
+            deletar_livro(isbn)
+
+        elif opcao == '5':
+            break
+
+        else:
+            print("Opção inválida! Tente novamente.")
