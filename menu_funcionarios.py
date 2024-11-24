f775cb76ca2e2280376feaab61a60c9585c70127
Adriano Muncaciu <muncaciua54@gmail.com>
2024-11-24 20:18:45 +0000

    GI

--- menu_funcionarios.py
+++ menu_funcionarios.py
@@ -1,6 +1,23 @@
 from Controller.Funcionarios import adicionar_funcionario, listar_funcionarios, atualizar_funcionario, deletar_funcionario, filtrar_funcionarios
 
 def gerenciar_funcionarios():
+    """
+    Interface de gerenciamento de funcionários.
+
+    Este método fornece um menu interativo para executar operações de CRUD (Criar, Ler, Atualizar, Deletar)
+    e filtrar funcionários, utilizando funções de um módulo controlador.
+
+    Funcionalidades disponíveis:
+        1. Adicionar funcionário
+        2. Listar funcionários
+        3. Atualizar informações de um funcionário
+        4. Deletar um funcionário
+        5. Filtrar funcionários por critérios
+        6. Voltar ao menu principal
+
+    O programa solicita entradas do usuário e chama funções adequadas para manipular os dados.
+
+    """
     while True:
         print("\nGerenciar Funcionários")
         print("1. Adicionar Funcionário")
