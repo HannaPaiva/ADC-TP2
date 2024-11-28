3e8a1c08fc2b362cd23fe53dbc89fb63c2e88424
Ferreira <paulocferreira@deloitte.pt>
2024-11-28 09:59:57 +0000

    Adicionadas docstrings em todas as funções para claridade de código, em livros.py: linha 70 foi retificado o erro que impedia a atualização de livros, foi adicionada a função de listar_livro_por_id, foi configurada para as duas funcoes de listar mostrar os registos em tabulate. em menu_livro.py foram adicionados blocos de codigo para tratamento de erro e alguma logica que impedia o funcionamento tambem foi mudada: proximo commit ira arranjar a funcao listar_livro_por_id

--- Controller/livros.py
+++ Controller/livros.py
@@ -3,6 +3,8 @@ from tabulate import tabulate
 import sqlite3
 
 def adicionar_livro(isbn, titulo, autor, categoria, ano_publicacao):
+    '''Função que tem como objetivo preparar e executar a query para inserir um livro novo na base de dados, com os valores de input introduzidos que originam de menu_livro.py, opção 1
+    Inclui tratamento de erro'''
     conexao = criar_conexao()
     cursor = conexao.cursor()
     try:
@@ -14,18 +16,39 @@ def adicionar_livro(isbn, titulo, autor, categoria, ano_publicacao):
     fechar_conexao(conexao)
 
 def listar_livro():
+    '''Função que tem como objetivo preparar e executar a query para listar todos os livros na base de dados, esta função é chamada através de menu_livro.py, opção 2
+    Inclui tratamento de erros e formatação de output com a biblioteca tabulate [pip install tabulate]'''
     conexao = criar_conexao()
     cursor = conexao.cursor()
-    cursor.execute("SELECT * FROM Livros")
-    livros = cursor.fetchall()
-    fechar_conexao(conexao)
-
-    headers = ["ISBN", "Título", "Autor", "Categoria", "Ano de Publicação"]
-    table = tabulate(livros, headers, tablefmt="grid")
+    try:
+        cursor.execute("SELECT * FROM Livros")
+        livros = cursor.fetchall()
+        fechar_conexao(conexao)
+        headers = ["ISBN", "Título", "Autor", "Categoria", "Ano de Publicação"]
+        table = tabulate(livros, headers, tablefmt="grid")
+    except sqlite3.Error as e:
+        print(f"Erro ao listar livros: {e}")
     return table
 
+def listar_livro_por_id(isbn):
+    '''Função que tem como objetivo preparar e executar a query para listar um dado livro na base de dados através do seu ISBN, esta função é chamada através de menu_livro.py, opção 2
+    Inclui tratamento de erros e formatação de output com a biblioteca tabulate [pip install tabulate]'''
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    try:
+        cursor.execute("SELECT * FROM Livros WHERE ISBN = ?", (isbn))
+        livros = cursor.fetchall()
+        fechar_conexao(conexao)
+        headers = ["ISBN", "Título", "Autor", "Categoria", "Ano de Publicação"]
+        table = tabulate(livros, headers, tablefmt="grid")
+    except sqlite3.Error as e:
+        print(f"Erro ao listar o livro especificado: {e}")
+    return table
+        
 
 def atualizar_livro(isbn, titulo=None, autor=None, categoria=None, ano_publicacao=None):
+    '''Função que tem como objetivo preparar e executar a query para atualizar um livro com base no ISBN na base de dados, com os valores de input introduzidos que originam de menu_livros.py, opção 3
+    Inclui tratamento de erros'''
     conexao = criar_conexao()
     cursor = conexao.cursor()
     campos = []
@@ -44,7 +67,7 @@ def atualizar_livro(isbn, titulo=None, autor=None, categoria=None, ano_publicaca
         campos.append("categoria = ?")
         valores.append(categoria)
     if ano_publicacao:
-        campos.append("ano publicacao = ?")
+        campos.append("ano_publicacao = ?")
         valores.append(ano_publicacao)
 
     if campos:
@@ -62,6 +85,8 @@ def atualizar_livro(isbn, titulo=None, autor=None, categoria=None, ano_publicaca
     fechar_conexao(conexao)
     
 def deletar_livro(isbn):
+    '''Função que tem como objetivo preparar e executar a query para apagar um livro com base no ISBN na base de dados, com o valor de input introduzidos que originam de menu_livros.py, opção 4
+    Inclui tratamento de erros'''
     conexao = criar_conexao()
     cursor = conexao.cursor()
     try:
